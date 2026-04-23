8c155a55082f4dc459bb4b5ed1990f145c9ecf4a
leegt <jimmy658437@gmail.com>
2026-04-23 15:36:09 +0800

    Initial Commit

---
+++ HW #4 Cygnus/HW__4_CygnusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW__4_CygnusApp.swift
+//  HW #4 Cygnus
+//
+//  Created by 114-2Workshop12 on 2026/4/23.
+//
+
+import SwiftUI
+
+@main
+struct HW__4_CygnusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
